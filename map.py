63c7e2a0d27699b7490340a18c011d822842b4c1
Ian Gill <iangill@gmail.com>
2024-02-26 07:58:43 -1000

    positron basemap

--- map.py
+++ map.py
@@ -3,7 +3,8 @@
 import folium
 
 m = folium.Map(location=(19.7216, -155.5),
-               zoom_start = 8)
+               zoom_start = 8,
+               tiles = 'cartodb positron')
 
 m.save('map.html')
 
